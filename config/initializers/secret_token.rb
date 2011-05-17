e18cbcc3d35c74ad59fded876c37563e7b40265c
Annika Shogren <annika.shogren@gmail.com>
2011-05-16 20:00:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovelyist::Application.config.secret_token = '0d8475a920de03639f2891693cce7ac0f3e2c6cc0c3882d110c81257b06e50a741572a57e4a00eee6f65e662a32261cc93ac6a4c2a49c08dde0861fcf8411b28'
