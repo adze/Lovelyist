ba79a4d1325f762580b62e4172e097f0c3f14635
Annika Shogren <annika.shogren@gmail.com>
2011-05-18 10:17:26 -0400

    Added Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Lovelyist::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
